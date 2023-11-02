4a7ce5ae23ee2632b6538fcdd79d3ebb67287d42
Felipe de Oliveira <felipe.oliveira@seasolutions.io>
2023-11-01 23:31:03 -0300

    teste

--- src/pages/api/generate-pdf/index.ts
+++ src/pages/api/generate-pdf/index.ts
@@ -56,7 +56,7 @@ export default async function handler(
       }
 
       console.log(process.env.AWS_LAMBDA_FUNCTION_VERSION);
-      const browser = await puppeteer.launch(options);
+      const browser = await puppeteerInstance.launch(options);
       const page = await browser.newPage();
       await page.goto("https://www.google.com");
       const title = await page.title();
